a469b9984fdfe6effe06c8d6ac7694a5e309e6c2
Ethan Marreel <ethan@marreel.com>
2026-06-02 10:59:40 -0700

    Darken AUI pane borders

--- kicad/project_tree_pane.cpp
+++ kicad/project_tree_pane.cpp
@@ -1618,8 +1618,8 @@ void PROJECT_TREE_PANE::onPaint( wxPaintEvent& event )
     wxRect    rect( wxPoint( 0, 0 ), GetClientSize() );
     wxPaintDC dc( this );
 
-    dc.SetBrush( wxSystemSettings::GetColour( wxSYS_COLOUR_FRAMEBK ) );
-    dc.SetPen( wxPen( wxSystemSettings::GetColour( wxSYS_COLOUR_ACTIVEBORDER ), 1 ) );
+    dc.SetBrush( KIPLATFORM::UI::GetPanelBGColour() );
+    dc.SetPen( wxPen( wxColour( 45, 45, 45 ), 1 ) );
 
     dc.DrawLine( rect.GetLeft(), rect.GetTop(), rect.GetLeft(), rect.GetBottom() );
     dc.DrawLine( rect.GetRight(), rect.GetTop(), rect.GetRight(), rect.GetBottom() );
